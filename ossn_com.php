05ab11964dc3f3106dc1de71b66f744f5b36b31b
Arsalan Shah <manactric@gmail.com>
2015-08-28 09:58:20 +0500

    Create ossn_com.php

---
+++ ossn_com.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @packageOpen Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+function custom_pagination_init(){
+	ossn_add_hook("pagination", "page_limit","set_new_page_limit"),
+}
+function set_new_page_limit(){
+   return 50;
+}
+ossn_register_callback('ossn', 'init', 'custom_pagination_init');
